79a9cc7b1a5cdb7e7a9db907c7c2fead5d1e5184
alxgmv <alxgmv@gmail.com>
2020-03-27 12:27:39 +0100

    1st

---
+++ data.js
@@ -0,0 +1,536 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--rez-de-chausse-1",
+      "name": " Rez-de-chaussée 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.3041215741818739,
+        "pitch": 0.031108564787725612,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.37022342765800254,
+          "pitch": 0.010025836944608457,
+          "rotation": 0,
+          "target": "1--rez-de-chausse-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.0921902326176482,
+          "pitch": -0.02517544682530115,
+          "title": "Bibliothèque GRYD",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/etagere/rs4u1BEqgh?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": 2.4586407069818996,
+          "pitch": -0.0923233784791222,
+          "title": "Chaise haute CHAYR",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/chaise-chayr/ck0a2AZVR?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": 2.2839271934391085,
+          "pitch": 0.004495057243989464,
+          "title": "Chaise CHAYR",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/chaise-chayr/Fi66HIIHV?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": 1.8284672385636211,
+          "pitch": 0.042186338775733745,
+          "title": "Buffet GRYD",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/etagere/L1y-GIg27?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": 1.7937142516415445,
+          "pitch": 0.5884328382719222,
+          "title": "Fauteuil PYLLOW",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/canape-pyllow/42cmuZ_ZY?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": 1.1870881040615924,
+          "pitch": 0.3727631940095115,
+          "title": "Bout de canapé SYDE",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/bout-de-canape/zHQbiPs0X?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": 0.876283964342953,
+          "pitch": 0.8602478980690016,
+          "title": "Table basse SYDE",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/bout-de-canape/F-X4sylAj?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": 0.1553164180159925,
+          "pitch": 0.3179984805920011,
+          "title": "Canapé TYME",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/canape-tyme/rVXW6qRUb?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": -1.4512188196840157,
+          "pitch": 0.4929100991975748,
+          "title": "Pouf TYME",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/canape-tyme/VsWfxJarab?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": -0.9844304874310446,
+          "pitch": 0.3703408283230587,
+          "title": "Table basse SYDE",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/bout-de-canape/ziWipbYAv?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": 0.7971601511693596,
+          "pitch": 0.14888141343105943,
+          "title": "Caisson sur roulettes GRYD",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/etagere/umWAmuaxr?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": 0.48232918167616745,
+          "pitch": -0.02830063245821357,
+          "title": "Console SYDE",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/bout-de-canape/z-KKS6Nye?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": 0.2695068933317337,
+          "pitch": 0.027894038440964053,
+          "title": "Chaise CHAYR",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/chaise-chayr/QHhSUcMWE?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": 0.03415685189738582,
+          "pitch": 0.12408165365331314,
+          "title": "Meuble bas GRYD",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/etagere/CMDELhfKi?tab=verification\">Ouvrir dans le configurateur</a>"
+        }
+      ]
+    },
+    {
+      "id": "1--rez-de-chausse-2",
+      "name": " Rez-de-chaussée 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.09223155893822899,
+          "pitch": 0.03718758220168539,
+          "rotation": 0,
+          "target": "0--rez-de-chausse-1"
+        },
+        {
+          "yaw": -3.0887168014657433,
+          "pitch": 0.11530988800252828,
+          "rotation": 0,
+          "target": "4-deuxime-tage-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.1890496850086798,
+          "pitch": 0.2221233339381108,
+          "title": "Table de travail TYMBER",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/table/GHRtD0j7k?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": -0.41152570089376184,
+          "pitch": 0.3587420781102537,
+          "title": "Chaise PRYME",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/chaise-pryme/QitqbTFIx?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": 0.09709675990220568,
+          "pitch": 0.15770782945140915,
+          "title": "Chaise STYNG",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/chaise-styng/F-8svFhHV?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": -0.052210899737751504,
+          "pitch": 0.17524635529432153,
+          "title": "Chaise STYNG",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/chaise-styng/Q-rxICFWd?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": 0.09260312918033442,
+          "pitch": 0.4557819869954933,
+          "title": "Chaise PRYME",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/chaise-pryme/QH_ClcngE?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": 0.41572416858793204,
+          "pitch": 0.38169868516570205,
+          "title": "Chaise PRYME",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/chaise-pryme/QwhfcGQWK?tab=couleurs\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": 1.1971160605951994,
+          "pitch": 0.2427915478812821,
+          "title": "Table à manger TYMBER",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/table/StROpQSt6?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": 0.9353997195901051,
+          "pitch": 0.40682660676628757,
+          "title": "Chaise PRYME",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/chaise-pryme/e_TnPxjef?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": 2.3515358855710673,
+          "pitch": 0.22469463990405458,
+          "title": "Étagère GRYD",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/etagere/h-phR-5-q?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": -1.0393665282253082,
+          "pitch": 0.11134861061340828,
+          "title": "Caisson sur roulettes GRYD",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/etagere/Q-VgGrsMZ?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": -1.3984853213006971,
+          "pitch": 0.0636172775567001,
+          "title": "Chaise STYNG",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/chaise-styng/Q-rxICFWd?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": -0.7188716354417188,
+          "pitch": 0.1998146598981787,
+          "title": "Meuble bas GRYD",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/etagere/CMDELhfKi?tab=verification\">Ouvrir dans le configurateur</a>"
+        }
+      ]
+    },
+    {
+      "id": "2-deuxime-tage-3",
+      "name": "Premier étage 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.18122418511981664,
+          "pitch": 0.064993444722095,
+          "rotation": 0,
+          "target": "3-deuxime-tage-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.49912919113365106,
+          "pitch": 0.21744586970154955,
+          "title": "Table TYMBER",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/table/SqdBul2gv?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": -0.44144326676837053,
+          "pitch": 0.41614914472225983,
+          "title": "Bout de canapé SYDE",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/bout-de-canape/FHWEev0nj?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": -1.117805072162895,
+          "pitch": 0.271077666826379,
+          "title": "Bureau TYMBER",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/table/pHIi1wYEE?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": 0.28429640138986123,
+          "pitch": 0.5463667863361117,
+          "title": "Convertible TYME",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/canape-tyme/7S7NX4mS3?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": 2.4136306750273535,
+          "pitch": 0.45710627387685676,
+          "title": "ECLYPSE Table basse",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/table-basse/QiW1dTc5?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": -2.269096888484251,
+          "pitch": 0.3084386159670096,
+          "title": "Dressing LYNE",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/dressing/H4sUC94N?tab=verification\">Ouvrir dans le configurateur</a>"
+        }
+      ]
+    },
+    {
+      "id": "3-deuxime-tage-2",
+      "name": "Premier étage 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.014620116559505902,
+          "pitch": 0.03301679936498303,
+          "rotation": 0,
+          "target": "2-deuxime-tage-3"
+        },
+        {
+          "yaw": 2.2821243997639478,
+          "pitch": 0.09701778634264713,
+          "rotation": 0,
+          "target": "4-deuxime-tage-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.9957554626553833,
+          "pitch": 0.29976432058616354,
+          "title": "Console SYDE",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/bout-de-canape/F-oXDOZRv?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": 1.6631139591294488,
+          "pitch": 0.361123758399188,
+          "title": "Bureau TYMBER",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/table/SIdnWoOE1?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": 1.1127586552926196,
+          "pitch": 0.4305977166134838,
+          "title": "Caisson sur roulettes GRYD",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/etagere/zHV1Z5Nav?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": -1.6826345783849632,
+          "pitch": 0.443646965375704,
+          "title": "Convertible TYME",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/canape-tyme/pHdaaMuFE?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": -0.9624058060027991,
+          "pitch": 0.35755381314202417,
+          "title": "Table basse SYDE",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/bout-de-canape/FwfQIr2vZ?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": 0.07527138948824863,
+          "pitch": 0.22143683720155494,
+          "title": "Chaise STYNG",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/chaise-styng/pwxef-nqd?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": -0.2060116452478553,
+          "pitch": 0.20718625514057898,
+          "title": "Chaise STYNG",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/chaise-styng/p-IKUyBrx?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": -0.1509730187990197,
+          "pitch": 0.5356608397324241,
+          "title": "Chaise STYNG",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/chaise-styng/pwWV_4BWd?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": -0.46992825136742766,
+          "pitch": 0.4351910368546932,
+          "title": "Chaise  CHAIR",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/chaise-chayr/5ma3Qd2a1?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": 0.18589575153439952,
+          "pitch": 0.36376225578148613,
+          "title": "Table TYMBER",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/table/RWU3Q_8PX?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": 0.33939114631396095,
+          "pitch": 0.25136310177017585,
+          "title": "Bout de canapé SYDE",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/bout-de-canape/FHWEev0nj?tab=verification\">Ouvrir dans le configurateur</a>"
+        }
+      ]
+    },
+    {
+      "id": "4-deuxime-tage-1",
+      "name": "Premier étage 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.48100463600884247,
+          "pitch": 0.015335632472147864,
+          "rotation": 0,
+          "target": "3-deuxime-tage-2"
+        },
+        {
+          "yaw": 2.2176035454024134,
+          "pitch": 0.1409539658104606,
+          "rotation": 0,
+          "target": "1--rez-de-chausse-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.82315424746571,
+          "pitch": 0.5261510887168761,
+          "title": "Convertible PYLLOW",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/canape-pyllow/Pjj_D3Ztp?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": -2.187980967602666,
+          "pitch": 0.4620502339650159,
+          "title": "Table basse ECLYPSE",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/table-basse/pwC-lbPyj?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": -1.7655427675548019,
+          "pitch": 0.5208924051354451,
+          "title": "Table basse ECLYPSE",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/table-basse/zi-9JjFM?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": -1.1948684497098743,
+          "pitch": 0.361376422950066,
+          "title": "Fauteuil TYME",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/canape-tyme/vemMXSNO5?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": -0.09027797517503622,
+          "pitch": 0.32213452908433027,
+          "title": "Canapé PYLLOW",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/canape-pyllow/Z6DyPagqm?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": 0.8431625181208275,
+          "pitch": 0.6338464956988545,
+          "title": "Table basse ECLYPSE",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/table-basse/L-nuHjCme?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": 0.31753349206172743,
+          "pitch": 0.7292576335080039,
+          "title": "Table basse ECLYPSE",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/table-basse/zHVmLldf?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": 0.6405247601849116,
+          "pitch": 0.3827041817217367,
+          "title": "Table basse ECLYPSE",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/table-basse/2wCRAK20c?tab=verification\">Ouvrir dans le configurateur</a>"
+        },
+        {
+          "yaw": 1.339109617350454,
+          "pitch": 0.6236603487063892,
+          "title": "Pouf PYLLOW",
+          "text": "<a target='_blank' href=\"https://fr.mycs.com/canape-pyllow/a7RaJOUiF?tab=verification\">Ouvrir dans le configurateur</a>"
+        }
+      ]
+    }
+  ],
+  "name": "Paris showroom",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
